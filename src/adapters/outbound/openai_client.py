4143d7adc8733cb25b2f54753ba8cfb236f95461
Nico <nicolas439@gmail.com>
2025-09-16 23:38:22 +0200

    added tweet output language var to the openai client

--- src/adapters/outbound/openai_client.py
+++ src/adapters/outbound/openai_client.py
@@ -88,7 +88,7 @@ class OpenAIClient(OpenAIPort):
         response = client.chat.completions.create(
             model=model,
             messages=[system_message, user_message],
-            temperature=0.9,            #  0.0 to 2.0 --> Nivel de creatividad/aleatoriedad. Valores bajos → respuestas más deterministas y “seguras”. Valores altos → más creatividad y variación, pero también más riesgo de desviarse del tema.
+            temperature=0.7,            #  0.0 to 2.0 --> Nivel de creatividad/aleatoriedad. Valores bajos → respuestas más deterministas y “seguras”. Valores altos → más creatividad y variación, pero también más riesgo de desviarse del tema.
             presence_penalty=0.3,       # -2.0 to 2.0 --> Penaliza o incentiva introducir nuevos temas no mencionados antes. Valores positivos → fomenta variedad temática. Valores negativos → favorece quedarse en los mismos temas.
             frequency_penalty=0.2       # -2.0 to 2.0 --> Penaliza o incentiva repetir las mismas palabras o frases. Valores positivos → reduce repeticiones. Valores negativos → permite o fomenta repeticiones.
         )
